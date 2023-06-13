998ea5f1ef8fd46df7acdb1a3758ccc0c8d375e7
olugbenga <gbejula@gmail.com>
2023-06-13 15:59:52 +0100

    routing table added

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "myapp-subnet-1" {
 resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
   route {
-    cidr_block = ""
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myapp-igw.id
   }
   tags = {
@@ -42,7 +42,6 @@ resource "aws_internet_gateway" "myapp-igw" {
 }
 
 resource "aws_route_table_association" "a-rtb-subnet" {
-  subnet_id = "aws.subnet.myapp-subnet-1.id"
+  subnet_id = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
-
 }
