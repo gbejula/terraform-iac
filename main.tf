89a1f51f1ab4ba049be99406ba09ef15d8d4772a
olugbenga <gbejula@gmail.com>
2023-06-16 14:56:44 +0100

    starting ec2 server

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
+variable instance_type {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_blocks
@@ -59,8 +60,7 @@ resource  "aws_default_route_table" "main_rtb" {
   }
 }
 
-resource "aws_security_group" "myapp-sg" {
-  name = "myapp-sg"
+resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
@@ -86,7 +86,7 @@ resource "aws_security_group" "myapp-sg" {
   }
 
   tags = {
-      Name: "${var.env_prefix}-sg"
+      Name: "${var.env_prefix}-default-sg"
     }
 }
 
@@ -95,7 +95,7 @@ data "aws_ami" "latest-amazon-linux-image" {
   owners = ["amazon"]
   filter {
     name = "name"
-    values = ["al2023-ami-2023-*-x86_64"]
+    values = ["al2023-ami-*-x86_64"]
   }
   filter {
     name = "virtualization-type"
@@ -107,9 +107,18 @@ output "aws_ami_id" {
   value = data.aws_ami.latest-amazon-linux-image
 }
 
-# resource "aws_instance" "myapp-server" {
-#   ami = data.ami.lastest-amazon-linux-image.id
-#   instance_type = var.instance_type
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
 
-#   subnet_id = aws_subnet.myapp-subnet-1
-# }
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = "server-key-pair"
+
+  tags = {
+    Name: "${var.env_prefix}-server"
+  }
+}
