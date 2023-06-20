3d97bc0c9102da602dd780a2f4152fa95057deb4
olugbenga <gbejula@gmail.com>
2023-06-20 17:12:06 +0100

    user data

--- main.tf
+++ main.tf
@@ -108,6 +108,8 @@ output "aws_ami_id" {
   value = data.aws_ami.latest-amazon-linux-image
 }
 
+user_data = file("entry-script.sh")
+
 resource "aws_key_pair" "ssh-key" {
   key_name = "server-key"
   public_key = file(var.public_key_location)
